a71a4d4bf21cd1366087f1a5ef0c3f5d71c1f2e5
“Jamilya <dzhumabekova@usf.edu>
2025-09-14 21:17:27 -0400

    step 2

---
+++ cc_3.js
@@ -0,0 +1,18 @@
+// create customer  records
+let customers = [
+  {
+    name: "jamilya one",
+    email: "jamilya@cat.com",
+    purchases: ["Laptop", "Headphones", "Coffee Mug"]
+  },
+  {
+    name: "jamilya two ",
+    email: "jamilya@dog.com",
+    purchases: ["Smartphone", "Wireless Charger"]
+  },
+  {
+    name: "jamilya three",
+    email: "jamilya@bat,com",
+    purchases: ["Desk Chair", "Notebook", "Pen Set", "Water Bottle"]
+  }
+];
